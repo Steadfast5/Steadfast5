cf3680f5e7d8f98ae0aa7f9445140fd93f30a52c
IceCruelStuff <minecraftcool991@gmail.com>
2020-01-25 19:57:55 -0800

    Release 1.5

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -72,7 +72,7 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = '1.4';
+	const VERSION = '1.5';
 	const API_VERSION = "1.12.0";
 	const CODENAME = "Server";
 	const MINECRAFT_VERSION = "v1.x";
