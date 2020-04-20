3ba91095bb5ad4a4c51ebeb126c73c037f678448
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-20 13:35:29 -0700

    fix cauldron tile

--- src/pocketmine/tile/Cauldron.php
+++ src/pocketmine/tile/Cauldron.php
@@ -10,23 +10,22 @@ use pocketmine\nbt\tag\StringTag;
 use pocketmine\tile\Spawnable;
 
 class Cauldron extends Spawnable {
-	
-	/** @todo add potionId */
-	/** @todo add splash potion checking */
-	/** @todo add isMovable logic */
-	
+
+	/** @TODO: add potionId */
+	/** @TODO: add splash potion checking */
+	/** @TODO: add isMovable logic */
+
 	public function getSpawnCompound() {
 		$compound = new Compound("", [
-            new StringTag("id", Tile::CAULDRON),
-            new IntTag("x", (int) $this->x),
-            new IntTag("y", (int) $this->y),
-            new IntTag("z", (int) $this->z),
-            new ShortTag("PotionId", -1),
-            new ByteTag("SplashPotion", 0),
-            new ByteTag("isMovable", 1),
-        ]);
-
+			new StringTag("id", Tile::CAULDRON),
+			new IntTag("x", (int) $this->x),
+			new IntTag("y", (int) $this->y),
+			new IntTag("z", (int) $this->z),
+			new ShortTag("PotionId", -1),
+			new ByteTag("SplashPotion", 0),
+			new ByteTag("isMovable", 1),
+		]);
 		return $compound;
 	}
-	
+
 }
