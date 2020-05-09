0ff36d4d67ecc8998cb7f3d36e0bcf9142a09ee8
IceCruelStuff <minecraftcool991@gmail.com>
2020-05-09 13:09:44 -0700

    small fix

--- src/pocketmine/inventory/DoubleChestInventory.php
+++ src/pocketmine/inventory/DoubleChestInventory.php
@@ -55,7 +55,7 @@ class DoubleChestInventory extends ChestInventory implements InventoryHolder{
 		return $index < $this->left->getSize() ? $this->left->getItem($index) : $this->right->getItem($index - $this->right->getSize());
 	}
 
-	public function setItem($index, Item $item) {
+	public function setItem($index, Item $item, $needCheckComporator = true) {
 		$old = $this->getItem($index);
 		if ($index < $this->left->getSize() ? $this->left->setItem($index, $item) : $this->right->setItem($index - $this->right->getSize(), $item)) {
 			$this->onSlotChange($index, $old);
