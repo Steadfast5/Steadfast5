58cce5d54af065941503e3c7241bd93e9060facc
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-18 19:07:57 -0700

    Update ItemFrameDropItemPacket.php

--- src/pocketmine/network/protocol/ItemFrameDropItemPacket.php
+++ src/pocketmine/network/protocol/ItemFrameDropItemPacket.php
@@ -4,7 +4,7 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
-class ItemFrameDropItemPacket extends PEPacket{
+class ItemFrameDropItemPacket extends PEPacket {
 
 	const NETWORK_ID = Info::ITEM_FRAME_DROP_ITEM_PACKET;
 	const PACKET_NAME = "ITEM_FRAME_DROP_ITEM_PACKET";
@@ -13,15 +13,17 @@ class ItemFrameDropItemPacket extends PEPacket{
 	public $y;
 	public $z;
 
-	public function decode($playerProtocol){
-        //$this->getHeader($playerProtocol);
+	public function decode($playerProtocol) {
+		//$this->getHeader($playerProtocol);
 		$this->getBlockPosition($this->x, $this->y, $this->z);
 		//$this->item = $this->getSlot($playerProtocol);
 	}
 
-	public function encode($playerProtocol){
-        //$this->reset($playerProtocol);
-        //$this->putBlockCoords($this->x, $this->y, $this->z);
-        //$this->setS
+	public function encode($playerProtocol) {
+		//$this->reset($playerProtocol);
+		//$this->putBlockCoords($this->x, $this->y, $this->z);
+		//$this->setS
+		$this->putBlockPosition($this->x, $this->y, $this->z);
 	}
+
 }
