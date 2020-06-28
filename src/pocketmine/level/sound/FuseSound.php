9007a5f3cc67d1cfada469900b75af74c890abb7
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-27 22:54:05 -0700

    Update FuseSound.php

--- src/pocketmine/level/sound/FuseSound.php
+++ src/pocketmine/level/sound/FuseSound.php
@@ -7,8 +7,10 @@ use pocketmine\network\protocol\LevelEventPacket;
 
 class FuseSound extends GenericSound {
 
+/*
 	public function __construct(Vector3 $pos, $pitch = 0) {
 		parent::__construct($pos, LevelEventPacket::FUSE_SOUND, $pitch);
 	}
+*/
 
 }
