d2ba5481ddf9dc0c18b19c53fb34e874837a5f91
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-08 20:41:16 -0700

    Update StainedClayMeta.php

--- src/pocketmine/level/generator/block/StainedClayMeta.php
+++ src/pocketmine/level/generator/block/StainedClayMeta.php
@@ -5,20 +5,20 @@ namespace pocketmine\level\generator\block;
 abstract class StainedClayMeta {
 
 	const CLAY_WHITE = 0;
-    const CLAY_ORANGE = 1;
-    const CLAY_MAGENTA = 2;
-    const CLAY_LIGHT_BLUE = 3;
-    const CLAY_YELLOW = 4;
-    const CLAY_LIME = 5;
-    const CLAY_PINK = 6;
-    const CLAY_GRAY = 7;
-    const CLAY_LIGHT_GRAY = 8;
-    const CLAY_CYAN = 9;
-    const CLAY_PURPLE = 10;
-    const CLAY_BLUE = 11;
-    const CLAY_BROWN = 12;
-    const CLAY_GREEN = 13;
-    const CLAY_RED = 14;
-    const CLAY_BLACK = 15;
+	const CLAY_ORANGE = 1;
+	const CLAY_MAGENTA = 2;
+	const CLAY_LIGHT_BLUE = 3;
+	const CLAY_YELLOW = 4;
+	const CLAY_LIME = 5;
+	const CLAY_PINK = 6;
+	const CLAY_GRAY = 7;
+	const CLAY_LIGHT_GRAY = 8;
+	const CLAY_CYAN = 9;
+	const CLAY_PURPLE = 10;
+	const CLAY_BLUE = 11;
+	const CLAY_BROWN = 12;
+	const CLAY_GREEN = 13;
+	const CLAY_RED = 14;
+	const CLAY_BLACK = 15;
 
 }
