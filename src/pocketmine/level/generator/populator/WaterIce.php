2601d58024194e6a923741d3f3728b0c3fbb167e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-08 21:02:43 -0700

    Create WaterIce.php

---
+++ src/pocketmine/level/generator/populator/WaterIce.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pocketmine\level\generator\populator;
+
+use pocketmine\level\ChunkManager;
+use pocketmine\utils\Random;
+
+class WaterIce extends Populator {
+
+	public function populate(ChunkManager $level, $chunkX, $chunkZ, Random $random) {
+		for ($x = 0; $x < 16; $x++) {
+			for ($z = 0; z < 16; $z++) {
+				
+			}
+		}
+	}
+
+}
