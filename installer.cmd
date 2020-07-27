04178f3bb282781915ccd645596333bb6c0dc157
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-27 16:02:03 -0700

    Update installer.cmd

--- installer.cmd
+++ installer.cmd
@@ -1,10 +1,12 @@
 @echo off
 
 ::
-::   ___ _____  ___  __ _ ___  ___  __ _  ___ _____   ____
-::  / __|__ __|/ _ \/ _` |   \|  _|/ _` |/ __|__ __| |  __|
-::  \__ \ | | |  __/ | | | | ||  _| | | |\__ \ | |   |__  \
-::  |___/ |_|  \___|\__,_|___/|_|  \__,_/|___/ |_|   |____/
+::   _____ _                 _  __          _   _____ 
+::  / ____| |               | |/ _|        | | | ____|
+:: | (___ | |_ ___  __ _  __| | |_ __ _ ___| |_| |__  
+::  \___ \| __/ _ \/ _` |/ _` |  _/ _` / __| __|___ \ 
+::  ____) | ||  __/ (_| | (_| | || (_| \__ \ |_ ___) |
+:: |_____/ \__\___|\__,_|\__,_|_| \__,_|___/\__|____/ 
 ::
 :: This program is free software: you can redistribute it and/or modify
 :: it under the terms of the GNU Lesser General Public License as published by
@@ -27,17 +29,12 @@ mkdir Steadfast5
 
 cd Steadfast5
 
-LOCATION=$(curl -s https://api.github.com/repos/Steadfast5/Steadfast5/releases/latest \
-| grep "tag_name" \
-| awk '{print "https://github.com/Steadfast5/Steadfast5/archive/" substr($2, 2, length($2)-3) ".zip"}') \
-; curl -L -o OUTPUT_FILE_NAME $LOCATION
+powershell -command "& { iwr https://github.com/Steadfast5/Steadfast5/releases/latest/download/Steadfast5.phar -OutFile Steadfast5.phar }"
 
-LOCATION=$(curl -s https://api.github.com/repos/Steadfast5/Steadfast5/releases/v1.3 \
-| grep "tag_name" \
-| awk '{print "https://github.com/Steadfast5/Steadfast5/releases/download/v1.1/installer.cmd" substr($2, 2, length($2)-3) ".zip"}') \
-; curl -L -o OUTPUT_FILE_NAME $LOCATION
+powershell -command "& { iwr https://jenkins.pmmp.io/job/PHP-7.3-Aggregate/lastSuccessfulBuild/artifact/PHP-7.3-Windows-x64.zip -OutFile PHP-7.3-Windows-x64.zip }"
+powershell -command "Expand-Archive -Path PHP-7.3-Windows-x64.zip -DestinationPath ."
 
-git clone https://github.com/IceCruelStuff/bin.git --recursive
+powershell -command "& { iwr https://raw.githubusercontent.com/Steadfast5/Installer-scripts/master/installer/start.cmd -OutFile start.cmd }"
 
 if exist bin\php\php.exe (
 	set PHPRC=""
