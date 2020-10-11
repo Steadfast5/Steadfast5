7476b281ff5aa5e952b307fe071df550e30159e0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-10 23:55:35 -0700

    Update Player.php

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1779,8 +1779,8 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer {
 		Item::COOKED_RABBIT => ['food' => 5, 'saturation' => 6],
 		Item::COOKED_SALMON => ['food' => 6, 'saturation' => 9.6],
 		Item::COOKIE => ['food' => 2, 'saturation' => 0.4],
-		Item::GOLDEN_APPLE => ['food' => 4, 'saturation' => 9.6],
-		Item::ENCHANTNED_GOLDEN_APPLE => ['food' => 4, 'saturation' => 9.6],
+//		Item::GOLDEN_APPLE => ['food' => 4, 'saturation' => 9.6],
+//		Item::ENCHANTNED_GOLDEN_APPLE => ['food' => 4, 'saturation' => 9.6], // crashes client
 		Item::GOLDEN_CARROT => ['food' => 6, 'saturation' => 14.4],
 		Item::MELON => ['food' => 2, 'saturation' => 1.2],
 		Item::MUSHROOM_STEW => ['food' => 6, 'saturation' => 7.2],
