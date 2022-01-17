da66e86a1f7a5ee7933c0a80d99a86a5e15dd8e9
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2022-01-17 13:55:56 +0700

    Update SetEntityDataPacket.php

--- src/pocketmine/network/protocol/SetEntityDataPacket.php
+++ src/pocketmine/network/protocol/SetEntityDataPacket.php
@@ -44,7 +44,7 @@ class SetEntityDataPacket extends PEPacket{
 		$this->putVarInt($this->eid);
 		$meta = Binary::writeMetadata($this->metadata, $playerProtocol);
 		$this->put($meta);
-		if ($playerProtocol >= Info::PROTOCOL_419) {
+		if ($playerProtocol >= Info::PROTOCOL_415) {
 			$this->putVarInt(0); // which tick from PlayerAuthInputPacket its on unsigned varint64
 		}
 	}
